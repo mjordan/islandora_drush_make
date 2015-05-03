21e794b30864ac032ccf7280f4f9497e3b9e5340
Mark Jordan <mjordan@sfu.ca>
2015-05-03 16:14:17 -0700

    Added simple shell script to enable all the modules vi drush.

---
+++ enable_islandora_modules.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Run this after running drush make --yes --no-core --contrib-destination=. islandora.drush.make
+# to install all the modules downloaded by that file.
+
+MAKEFILE=$1
+
+# Extract all the module names from the islandora makefile.
+MODULES=$(grep -oh "projects\[[A-Za-z_]*\]" $MAKEFILE | sed "s/projects\[//" | sed "s/\]/ /" | tr -d '\n')
+
+# Just echo for now, don't actually run it (pending testing).
+echo "drush -yes $MODULES"
